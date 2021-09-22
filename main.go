352602cffb633bd86cd2c261e6b0b060f0c26fad
DESKTOP-P390N85\Владимир <vova.kuznetsov4@mail.ru>
2021-09-22 19:14:52 +0300

    Just creating rep

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	//"os"
+	"fmt"
+	//"bufio"
+)
+
+func main(){
+	//in := bufio.NewScanner(os.Stdin)
+	//for in.Scan() {
+	//	txt := in.Text()
+	//	fmt.Println(txt)
+	fmt.Println("Hello, world!")
+	//}
+}
