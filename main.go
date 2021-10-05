0692243ba44fe30abb70dd95b115bb9f0d1279e1
DESKTOP-P390N85\Владимир <vova.kuznetsov4@mail.ru>
2021-10-05 18:05:56 +0300

    For sorted files

--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
-	//"os"
+	"os"
 	"fmt"
-	//"bufio"
+	"bufio"
 )
-
 func main(){
-	//in := bufio.NewScanner(os.Stdin)
-	//for in.Scan() {
-	//	txt := in.Text()
-	//	fmt.Println(txt)
-	fmt.Println("Hello, world!")
-	//}
+	in := bufio.NewScanner(os.Stdin)
+	var prev string
+
+	for in.Scan() {
+		txt := in.Text()
+		if txt == prev{
+			continue
+		}
+		if txt < prev{
+			panic("file not sorted")
+		}
+		prev = txt
+		fmt.Println(txt)
+	}
 }
